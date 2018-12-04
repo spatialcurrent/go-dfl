bd3bd265206237aa83529367c1c6fdb84f4342a1
Patrick Dufour <pjdufour.dev@gmail.com>
2018-12-03 19:35:32 -0500

    added MustParseCompileEvaluate

---
+++ dfl/MustParseCompileEvaluate.go
@@ -0,0 +1,18 @@
+// =================================================================
+//
+// Copyright (C) 2018 Spatial Current, Inc. - All Rights Reserved
+// Released as open source under the MIT License.  See LICENSE file.
+//
+// =================================================================
+
+package dfl
+
+// MustParseCompileEvaluate parses the expression, compiles the node, and evaluates on the given context.  Panics if any error.
+func MustParseCompileEvaluate(exp string, vars map[string]interface{}, ctx interface{}, funcs FunctionMap, quotes []string) (map[string]interface{}, interface{}) {
+	vars, result, err := ParseCompileEvaluate(exp, vars, ctx, funcs, quotes)
+	if err != nil {
+		panic(err)
+	}
+
+	return vars, result
+}
